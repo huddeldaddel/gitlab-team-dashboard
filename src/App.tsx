8aa09fb1fffac6f7ff9a334c2694443bc26e1f3f
huddeldaddel <35447824+huddeldaddel@users.noreply.github.com>
2023-02-16 23:54:01 +0100

    Added gitbeaker to access GitLab's API

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import PageFooter from './components/page-footer'
 import ConfigPage from './pages/config/Config'
+import PageFooter from './components/page-footer'
+import TestPage from './pages/test/Test'
 import { ConfigService } from './services/config-service'
 import { DashboardConfig } from './model/dashboard-config';
 
@@ -17,24 +18,26 @@ interface IState {
 }
 
 class App extends React.Component<IProps, IState> {
-  
+
   constructor(props: IProps) {
     super(props);
     this.state = {
       config: new ConfigService().LoadConfig()
-    };  
+    };
   }
 
   render() {
     var component = <div />;
 
-    if(!this.state.config) {
+    if (!this.state.config) {
       component = <ConfigPage />
+    } else {
+      component = <TestPage config={this.state.config} />
     }
 
     return (
       <div className="App">
-        { component }
+        {component}
         <PageFooter />
       </div>
     );
---
+++ src/pages/test/Test.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { ConfigService } from '../../services/config-service'
+import { DashboardConfig } from '../../model/dashboard-config';
+import { GitLabConfig } from '../../model/gitlab-config';
+import { Gitlab } from '@gitbeaker/browser'; // All Resources
+import { ProjectSchema } from '@gitbeaker/core/dist/types/resources/Projects'
+
+import './Test.css';
+
+interface IProps {
+    config: DashboardConfig
+}
+
+interface IState {
+
+}
+
+class TestPage extends React.Component<IProps, IState> {
+
+    constructor(props: IProps) {
+        super(props);
+        this.state = {};
+        this.handleGetSpecificProject = this.handleGetSpecificProject.bind(this);
+        this.handleGetProjectsOfGroup = this.handleGetProjectsOfGroup.bind(this);
+    }
+
+    async handleGetSpecificProject(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {        
+        try {
+            const api = new Gitlab({
+                host: this.props.config.gitlab?.host,
+                token: this.props.config.gitlab?.token,
+            });
+
+            // const project = await api.Projects.show(43595685);
+            const project = await api.Projects.all();
+            console.log("Received project: " + JSON.stringify(project));            
+        } catch (error) {
+            console.error("Failed to load errors: " + error);
+        }
+    }
+
+    async handleGetProjectsOfGroup(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+        console.debug("Now trying to access " + this.props.config.gitlab?.host);
+
+        try {
+            const api = new Gitlab({
+                host: this.props.config.gitlab?.host,
+                token: this.props.config.gitlab?.token,
+            });
+
+            const group = await api.Groups.show(63977505);
+            console.log("Received group: " + JSON.stringify(group));            
+        } catch (error) {
+            console.error("Failed to load errors: " + error);
+        }
+    }
+
+    render() {
+        return (
+            <div className="TestPage">
+                <div className="control">
+                    <button className="button is-link is-light" onClick={this.handleGetSpecificProject}>Get specific project</button>
+                </div>
+
+                <div className="control">
+                    <button className="button is-link is-light" onClick={this.handleGetProjectsOfGroup}>Get projects of group</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default TestPage;
